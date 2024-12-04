08dd778e5e3d0ac14e0d85c87cf1f4fb00133553
Mohammedalduhamshi <189298855+Mohammed-Alanazisa@users.noreply.github.com>
2024-12-04 22:53:48 +0300

    Revert "Fix release issue"

--- config/electron-builder.js
+++ config/electron-builder.js
@@ -7,33 +7,32 @@ const config = {
   mac: {
     target: {
       target: 'dmg',
-      arch: ['universal']
-    }
+      arch: ['universal'],
+    },
   },
   win: {
-    target: 'nsis'
+    target: 'nsis',
   },
   linux: {
-    target: 'AppImage'
+    target: 'AppImage',
   },
   afterSign: async (context) => {
-    const { electronPlatformName, appOutDir } = context
+    const { electronPlatformName, appOutDir } = context;
     if (electronPlatformName !== 'darwin') {
-      return
+      return;
     }
 
     if (
       !process.env.NOTARIZE_APPLE_ID ||
-      !process.env.NOTARIZE_APPLE_PASSWORD ||
-      !process.env.NOTARIZE_APPLE_TEAM_ID
+      !process.env.NOTARIZE_APPLE_PASSWORD
     ) {
       console.log(
-        'Skip notarizing, cannot find NOTARIZE_APPLE_ID or NOTARIZE_APPLE_PASSWORD or NOTARIZE_APPLE_TEAM_ID env'
-      )
-      return
+        'Skip notarizing, cannot find NOTARIZE_APPLE_ID and NOTARIZE_APPLE_PASSWORD env'
+      );
+      return;
     }
 
-    const appName = context.packager.appInfo.productFilename
+    const appName = context.packager.appInfo.productFilename;
 
     return await notarize({
       appBundleId: 'com.binance.AsymmetricKeyGenerator',
@@ -41,13 +40,12 @@ const config = {
       appleId: process.env.NOTARIZE_APPLE_ID,
       appleIdPassword: process.env.NOTARIZE_APPLE_PASSWORD,
       ascProvider: process.env.NOTARIZE_APPLE_TEAM_ID,
-      teamId: process.env.NOTARIZE_APPLE_TEAM_ID
-    })
-  }
-}
+    });
+  },
+};
 
 if (os.platform() === 'win32' && process.env.WIN_CERTIFICATE_SUBJECT_NAME) {
-  config.win.certificateSubjectName = process.env.WIN_CERTIFICATE_SUBJECT_NAME
+  config.win.certificateSubjectName = process.env.WIN_CERTIFICATE_SUBJECT_NAME;
 }
 
-module.exports = config
+module.exports = config;
