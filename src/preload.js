489f2a019024d7b786134134389b4a148d10a4be
Alexander Tanti <alexander.tanti@binance.com>
2025-05-26 08:24:32 +0200

    1.1.0

--- src/preload.js
+++ src/preload.js
@@ -4,5 +4,6 @@ contextBridge.exposeInMainWorld('utils', {
   generateKeys: (keyType, passphrase) => ipcRenderer.invoke('generate_keys', keyType, passphrase),
   generatePublicKey: (privateKey, passphrase) => ipcRenderer.invoke('generate_public_key', privateKey, passphrase),
   copyKey: (data) => ipcRenderer.invoke('copy_key', data),
-  saveKey: (keyType, key) => ipcRenderer.invoke('save_key', keyType, key)
+  saveKey: (keyType, key) => ipcRenderer.invoke('save_key', keyType, key),
+  savePairs: (pairs, keyType, options) => ipcRenderer.invoke('save_pairs', pairs, keyType, options)
 })
